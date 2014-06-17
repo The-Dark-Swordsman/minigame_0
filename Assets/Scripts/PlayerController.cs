30733ac2b25eb67f35a0cece27bf542cfcce54d1
The-Dark-Swordsman <thedarkswordsman54321@gmail.com>
2014-06-17 02:14:04 -0400

    All Game Files
    All Game Files for "Collect Them Balls! v0.01.1" and "Ball Collect
    Test."

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed;
+	public GUIText countText;
+	public GUIText winText;
+	public GUIText descText;
+	private int count; 
+	
+
+	void Start ()
+	{
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+	
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+	Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		rigidbody.AddForce(movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "PickUp")
+		{
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if(count >= 0)
+		{
+			descText.text = "C'mon Hurry Up";
+		}
+		if(count >= 1)
+		{
+			descText.text = "You Suck.";
+		}
+		if(count >= 4)
+		{
+			descText.text = "Okay...";
+		}
+		if(count >= 7)
+		{
+			descText.text = "Alright, You're Getting There";
+		}
+		if(count >= 10)
+		{
+			descText.text = "ALMOST THERE!";
+		}
+		if(count >= 13)
+		{
+			winText.text = "YOU WIN!";
+			descText.text = "Good Job ^_^";
+		}
+	}
+}
